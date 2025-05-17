49277fc923341de1341ff42d4733e0f68a26d76e
Ivan Marianek <ivan.marianekgmail.com>
2025-05-17 12:03:21 +0200

    CSS responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,6 +3,7 @@ export function Footer() {
     return (
         <footer>
             <p>2025 CheckReddit. All rights reserved.</p>
+            <p>Author: Ivan Marianek</p>
         </footer>
     );
 }
